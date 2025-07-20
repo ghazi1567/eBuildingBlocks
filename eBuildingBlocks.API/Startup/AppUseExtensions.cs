a9e2a3c367c5f2186b0bc180b6f4b264ee6a82c7
Inam Ul Haq <i.ulhaq@rsof.com.sa>
2025-07-20 22:10:54 +0500

    Locale changes v1

--- eBuildingBlocks.API/Startup/AppUseExtensions.cs
+++ eBuildingBlocks.API/Startup/AppUseExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using eBuildingBlocks.Application;
 using eBuildingBlocks.Application.Middlewares;
-using eBuildingBlocks.Domain.Enums;
 using Hangfire;
 using Hangfire.Dashboard;
 using HealthChecks.UI.Client;
@@ -62,16 +61,16 @@ public static class AppUseExtensions
                 }
             });
         });
-        
+
         var pathBase = configuration.GetValue<string>("PathBase");
-        #if DEBUG
-                Console.WriteLine("This code runs only in Debug build.");
-        #else
+#if DEBUG
+        Console.WriteLine("This code runs only in Debug build.");
+#else
                 if (string.IsNullOrEmpty(pathBase))
                 {
                     throw new ArgumentNullException("PathBase", "PathBase is not configured in appsettings.json or environment variables.");
                 }
-        #endif
+#endif
 
 
         app.UseSwaggerUI(options =>
